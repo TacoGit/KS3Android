cd80c98e0545220dc0f87ca43dbeed3f0cc6300b
tanos <68923270+TacoGit@users.noreply.github.com>
2024-05-05 13:18:16 +0200

    Android Support implemented
    YAAAAAAAAAAAAAAAAAAAAHOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO

---
+++ Assets/AndroidCameraLock.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndroidCameraLock : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//var lockFunction = window.screen.orientation.lock;
+		//if (lockFunction.call(window.screen.orientation, 'landscape')) {
+		//} else {
+		//}
+	}
+}
